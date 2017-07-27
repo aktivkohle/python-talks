9ffeacbab3330f1a0dba89e871e34b76b4a43c53
felix <felix.s@posteo.de>
2017-07-27 18:54:35 +0200

    changed number of neurons to more appropriate 300 given 462 input and 18 output neurons. 8 was too few

--- generate_responses.py
+++ generate_responses.py
@@ -27,8 +27,8 @@ with open(INTENTSFILE) as json_data:
     
 # Build neural network
 net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 300)
+net = tflearn.fully_connected(net, 300)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
 
--- train_model.py
+++ train_model.py
@@ -136,8 +136,8 @@ if __name__ == "__main__":
     tf.reset_default_graph()
     # Build neural network
     net = tflearn.input_data(shape=[None, len(train_x[0])])
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 300)
+    net = tflearn.fully_connected(net, 300)
     net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
     net = tflearn.regression(net)
 
